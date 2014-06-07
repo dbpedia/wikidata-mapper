66aa91368950d38013c11e39fa776b2bb6af0807
Sergey Skovorodkin <sergey@skovorodkin.com>
2014-06-08 03:09:54 +0400

    Delete unused imports

--- dump_dbpedia_properties_and_classes.py
+++ dump_dbpedia_properties_and_classes.py
@@ -4,11 +4,8 @@ from __future__ import (
     absolute_import, division, print_function, unicode_literals
 )
 
-import bz2
 import json
 import os
-from datetime import datetime
-from itertools import ifilterfalse
 
 import requests
 
